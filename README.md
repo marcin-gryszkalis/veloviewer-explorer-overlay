ab429686140dd59d8bf0700819ce15cee97bbb11
Marcin Gryszkalis <mg@fork.pl>
2020-07-12 15:11:25 +0200

    support for zoom levels < 14

--- README.md
+++ README.md
@@ -10,10 +10,11 @@ Generic overlay for VeloViewer Explorer squares
 ![brouter with overlay](brouter-example.png "brouter with overlay!")
 
 ## TODO
-- [ ] zoom level < 14
+- [x] zoom level < 14
 - [ ] color setup support (maybe including transparency option)
 - [x] frames
 - [ ] auto-refresh of squares cache, with proper locking
 - [ ] cache (at least for z=14+ and later for z=11..13)
+- [ ] largest square support (clashes with generic cachce, maybe as another overlay???)
 - [ ] different color for clusters ("inside" squares)
 
--- tile.php
+++ tile.php
@@ -8,8 +8,7 @@ $id = intval($_GET['id']);
 header('Content-type: image/png');
 
 error_log("id($id) $x:$y @ $z");
-# TODO: support for zoom<14 (buid image)
-if ($z < 14 || $id == 0 || !file_exists("cache/$id.php"))
+if ($z < 7 || $id == 0 || !file_exists("cache/$id.php")) # z7 is lowest level that makes sense to display
 {
     readfile("empty256x256.png");
     exit();
@@ -37,16 +36,54 @@ $png = imagecreatefrompng("empty256x256.png");
 imagesavealpha($png, true);
 $c_bg = imagecolorallocatealpha($png, 255, 0, 0, 100);
 $c_frame = imagecolorallocatealpha($png, 255, 0, 0, 50);
-
-# error_log("$x:$y = ".array_key_exists("$x:$y", $exp));
-
-if (array_key_exists("$x:$y", $exp))
+ 
+if ($z >= 14)
 {
-    imagefilledrectangle($png, 0, 0, 255, 255, $c_bg);
+    # error_log("$x:$y = ".array_key_exists("$x:$y", $exp));
+    
+    if (array_key_exists("$x:$y", $exp))
+    {
+        imagefilledrectangle($png, 0, 0, 255, 255, $c_bg);
+    }
+    
+    if ($f_l) imageline($png, 0, 0, 0, 255, $c_frame);
+    if ($f_t) imageline($png, 0, 0, 255, 0, $c_frame);
+    
+}
+elseif ($z < 14) # lower limit is checked before
+{
+    $dz = 14 - $z;
+    $zm = 2 ** $dz; # zoom multiplier
+    $r = 256 / $zm; # small square size
+
+    $jx = 0;
+    foreach (range($x*$zm, $x*$zm + $zm - 1) as $ix)
+    {
+        $jy = 0;
+        foreach (range($y*$zm, $y*$zm + $zm - 1) as $iy)
+        {
+#            error_log("z:$z $ix:$iy = ".array_key_exists("$ix:$iy", $exp));
+            if (array_key_exists("$ix:$iy", $exp))
+            {
+                imagefilledrectangle($png, 
+                    $r * $jx, $r * $jy, 
+                    $r * $jx + $r - 1, $r * $jy + $r - 1, 
+                    $c_bg);
+            }
+            $jy++;      
+        }
+        $jx++;
+    }
+
+    if ($z >= 10) # no frames for lower zoom levels
+    {
+        foreach (range(0, $zm - 1) as $i)
+        {
+            imageline($png, $i * $r, 0, $i * $r, 255, $c_frame);
+            imageline($png, 0, $i * $r, 255, $i * $r, $c_frame);
+        }
+    }
+ 
 }
 
-if ($f_l) imageline($png, 0, 0, 0, 255, $c_frame);
-if ($f_t) imageline($png, 0, 0, 255, 0, $c_frame);
-
 imagepng($png);
-
